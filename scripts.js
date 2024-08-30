c98f5093ff11739b9e0e5fedb937ae72043b9cd6
namansahani <153166671+namansahani@users.noreply.github.com>
2024-08-30 23:24:57 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const button = document.getElementById('alertButton');
+    
+    button.addEventListener('click', () => {
+    button.addEventListener('more image', () => {
+        alert('Welcome to NGP Wood!');
+    });
+});
